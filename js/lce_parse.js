4c8051d46a8298c7147403896c65dc4d82383e3a
Nick Nygren <nnygren@shaw.ca>
2013-06-01 12:24:46 -0600

    parsing expressions which contain parenthesis

--- js/lce_parse.js
+++ js/lce_parse.js
@@ -1,5 +1,5 @@
 
-
+// strips whitespace from strings
 function strip_ws(input)
 {
 	if (typeof input != 'string')
@@ -10,7 +10,35 @@ function strip_ws(input)
 	return input.replace(/^\s*/, "").replace(/\s*$/, "");
 }
 
+// traverses the string, starting with an open parenthesis, finds the point where it is closed.
+function findCloseBalance(input, start)
+{
+	if (typeof input != 'string')
+	{
+		return -1;
+	}
 
+	if (input[start] != '(')
+	{
+		return start;
+	}
+
+	var i;
+	var depth = 1;
+
+	for (i = start + 1; i < input.length; i++)
+	{
+		if (input[i] == '(') { depth++; }
+		else if (input[i] == ')') { depth--; }
+
+		if (depth == 0)
+		{
+			return i;
+		}
+	}
+
+	return -1;
+}
 
 // Lambda calculus expression "variable" type
 function lce_expr_var(input)
@@ -72,12 +100,14 @@ function lce_expr_app(inlhs, inrhs)
 	}
 }
 
+// class for storing mode/expression pairs in the parser's stack
 function stack_pair(inmode, inexpr)
 {
 	this.mode = inmode;
 	this.expr = inexpr;
 }
 
+// The main parser method. Takes a string and returns a tree of lce_expr_app, lce_expr_abs, and lce_expr_var objects
 function lce_read_expr(input)
 {
 	if  (typeof input != 'string')
@@ -85,12 +115,12 @@ function lce_read_expr(input)
 		return undefined;
 	}
 
-	var i;
-	var top;
+	var i,j;
 	var current = undefined;
+	var temp;
 	var stack = [];
-	var MODE_EXPR = 0;
-	var MODE_VARLIST = 1;
+	var MODE_EXPR = 0; // main mode, determines all type for all other expressions
+	var MODE_VARLIST = 1; // handles the function abstraction type, which has a restricted character set(meta-meanings)
 	var mode = MODE_EXPR;
 	var varstart = 0;
 	
@@ -98,7 +128,29 @@ function lce_read_expr(input)
 	{
 		if (mode == MODE_EXPR)
 		{
-			if (input[i] == '\\')
+		//recursively handle any parenthesis
+			if (input[i] == '(')
+			{
+				j = findCloseBalance(input, i);
+				if (j == -1)
+				{
+					alert("Error: imbalance of parenthesis.");
+					return undefined;
+				}
+				temp = lce_read_expr(input.substr(i+1, j-i-1));
+				if (current == undefined)
+				{
+					current = temp;
+				}
+				else
+				{
+					current = new lce_expr_app(current, temp);
+				}
+				i = j;
+				varstart = i + 1;
+			}
+			// detect the beginning of a functional abstraction
+			else if (input[i] == '\\')
 			{
 				if (input.substr(i+1,6) == 'lambda')
 				{
@@ -109,6 +161,7 @@ function lce_read_expr(input)
 					varstart = i;
 				}
 			}
+			// look for variable names and applications
 			else if ((input[i].match(/\s/) && strip_ws(input.substr(varstart,i+1-varstart)) != "")
 				|| (i + 1 == input.length && strip_ws(input.substr(varstart,i+1-varstart)) != ""))
 			{
@@ -125,6 +178,7 @@ function lce_read_expr(input)
 		}
 		else if (mode == MODE_VARLIST)
 		{
+			//detect the end of the variable list
 			if (input[i] == '.')
 			{
 				if (current.varlist.length == 0)
@@ -137,11 +191,13 @@ function lce_read_expr(input)
 				current = undefined;
 				varstart = i + 1;
 			}
+			// detect illegal characters
 			else if (input[i] == '\\' || input[i] == '(' || input[i] == ')')
 			{
 				alert("Error: Illegal character, '\\', '(', ')' in variable list.");
 				return undefined;
 			}
+			// detect variable names and add to list
 			else if ((input[i].match(/\s/) && strip_ws(input.substr(varstart,i+1-varstart)) != "")
 				|| ((i + 1 == input.length || input[i + 1] == '.') && strip_ws(input.substr(varstart,i+1-varstart)) != ""))
 			{
@@ -151,6 +207,7 @@ function lce_read_expr(input)
 		}
 	}
 
+	// unstack all stored partial results and combine into final result
 	while (stack.length > 0)
 	{
 		if (stack[stack.length - 1].mode == MODE_EXPR)
